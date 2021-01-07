3559a42d3ba7548d6064a8b8073aa737c1dfccde
John Byrne <jbyrne87@gmail.com>
2021-01-07 10:31:38 -0600

    still resolving heroku issue

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   JWT_SECRET: process.env.JWT_SECRET || 'my own secret',
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/map_mash',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/map_mash',
 }
